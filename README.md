e2cdecfffee744270f13c2f6775b7528af569713
trini-tA <trini-tA>
2020-06-06 15:29:16 +0200

    Add User and seeder

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
-# laravAdmin
+laravAdmin
 
+# install
+1. composer install
+2. npm install
+3. php artisan migrate
+
+
+
+## init db
+php artisan migrate:fresh --seed
+
+# Ressources on web
+- https://laravel.com/docs
+- http://laravel.sillo.org/
 
 # Tools & Addoons
+- https://github.com/barryvdh/laravel-debugbar
 - 
---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(){
+        DB::table('users')->insertOrIgnore([
+            'name' => 'administrator',
+            'email' => 'administrator@localhost',
+            'password' => Hash::make('password'),
+        ]);
+    }
+}
