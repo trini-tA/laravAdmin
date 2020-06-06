d86047f32a74ce94466923a20dbfecc853582393
trini-tA <trini-tA>
2020-06-06 15:41:05 +0200

    disable register and change /

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['register' => false]);
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
