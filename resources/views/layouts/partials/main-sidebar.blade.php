9d900d6fc4db7d651372862dbe24edec3d7c3a20
trini-tA <trini-tA>
2020-07-01 23:26:16 +0200

    Add menu open & active

--- resources/views/layouts/partials/main-sidebar.blade.php
+++ resources/views/layouts/partials/main-sidebar.blade.php
@@ -1,4 +1,4 @@
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+<aside class="main-sidebar sidebar-light-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ route('admin') }}" class="brand-link">
         <img src="{{ asset( '/adminlte/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
--- resources/views/layouts/partials/sidebar-menu.blade.php
+++ resources/views/layouts/partials/sidebar-menu.blade.php
@@ -1,7 +1,7 @@
 <!-- Sidebar Menu -->
 <nav class="mt-2">
     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-      <li class="nav-item has-treeview">
+      <li class="nav-item has-treeview {{menuOpen('user.index', 'role.index', 'permission.index', 'assignment.index' )}}">
 
         @role( 'superadministrator' )
           <a href="#" class="nav-link">
@@ -9,16 +9,16 @@
             <p>{{ __( 'Administration') }}<i class="right fas fa-angle-left"></i></p>
           </a>
           <ul class="nav nav-treeview">
-            <x-menu-item href="{{ route( 'user.index') }}" :sub=true :active=false>
+            <x-menu-item href="{{ route( 'user.index') }}" :sub=true :active="currentRouteActive('user.index')">
               {{ __( 'Users' ) }}
             </x-menu-item>
-            <x-menu-item href="{{ route( 'role.index') }}" :sub=true :active=false>
+            <x-menu-item href="{{ route( 'role.index') }}" :sub=true :active="currentRouteActive('role.index')">
               {{ __( 'Roles' ) }}
             </x-menu-item>
-            <x-menu-item href="{{ route( 'permission.index') }}" :sub=true :active=false>
+            <x-menu-item href="{{ route( 'permission.index') }}" :sub=true :active="currentRouteActive('permission.index')">
               {{ __( 'Permissions' ) }}
             </x-menu-item>
-            <x-menu-item href="{{ route( 'assignment.index') }}" :sub=true :active=false>
+            <x-menu-item href="{{ route( 'assignment.index') }}" :sub=true :active="currentRouteActive('assignment.index')">
               {{ __( 'assignment' ) }}
             </x-menu-item>
           </ul>
@@ -26,10 +26,10 @@
 
       </li>
       @role( 'superadministrator' )
-        <x-menu-item href="{{ route( 'logs.index') }}" icon="fa fa-fw fa-bug" :active=false>
+        <x-menu-item href="{{ route( 'logs.index') }}" icon="fa fa-fw fa-bug" :active="currentRouteActive('logs.index')">
           {{ __( 'Logs' ) }}
         </x-menu-item>
-        <x-menu-item href="{{ route( 'activity_log.index') }}" icon="fa fa-fw fa-bug" :active=false>
+        <x-menu-item href="{{ route( 'activity_log.index') }}" icon="fa fa-fw fa-bug" :active="currentRouteActive('activity_log.index')">
           {{ __( 'Activity logs' ) }}
         </x-menu-item>
       @endrole
